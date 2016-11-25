3f68f1c9d0929087e9aa43143d6b2ebac6e89ad0
Pranitagandhi <gandhipranita@gmail.com>
2016-11-25 10:01:54 +0530

    Update MinString.java

--- src/MinString.java
+++ src/MinString.java
@@ -1,51 +1,66 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
+
+
+import java.util.Scanner;
 
 public class MinString {
-	static int min(int x, int y, int z) {
-		if (x < y && x < z)
-			return x;
-		if (y < x && y < z)
-			return y;
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String s1,s2;
+		
+		System.out.println("Enter the string:");
+		s1=sc.next();
+		System.out.println("Enter secnd string");
+		s2=sc.next();
+		System.out.println("string s1 : "+s1);
+		System.out.println("string s2 : "+s2);
+		int max=0,min=0,l1,l2;
+		l1=s1.length();
+		l2=s2.length();
+		char[] a1=s1.toCharArray();
+		char[] a2=s2.toCharArray();
+		char[]a3= new char[10];
+		String s3 = null;
+		int p=0;
+		
+		if(l1>l2)
+		{
+			max=l1;
+			for (int i = 0; i < s2.length(); i++) {
+				/*for (int j = 0; j < s2.length(); j++) {*/
+					if(!(s1.substring(i, i+1).equalsIgnoreCase(s2.substring(i, i+1))))
+					{
+						a1[i]=a2[i];
+						a3[p]=a2[i];
+						s3=s3+s2.substring(i, i+1);
+						p++;
+						System.out.println(a3[p]);
+						min=i;
+					}
+				/*}*/
+	
+			}
+				s1=s1.substring(0, s2.length());
+		}
 		else
-			return z;
-	}
-
-	static int editDist(String str1, String str2, int m, int n) {
-
-		if (m == 0) {
-
-			return n;
+		{
+			max=l2;
 		}
-
-		if (n == 0)
-			return m;
-
-		if (str1.charAt(m - 1) == str2.charAt(n - 1))
-			return editDist(str1, str2, m - 1, n - 1);
-
-		return 1 + min(editDist(str1, str2, m, n - 1), // Insert
-				editDist(str1, str2, m - 1, n), // Remove
-				editDist(str1, str2, m - 1, n - 1) // Replace
-		);
-	}
-
-	public static void main(String args[]) {
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					System.in));
-			System.out.println("Enter first string: ");
-			String str1 = br.readLine();
-
-			System.out.println("Enter Second String:");
-			String str2 = br.readLine();
-
-			System.out.println(editDist(str1, str2, str1.length(),
-					str2.length()));
-
-		} catch (IOException e) {
-			System.out.println(" ");
+		System.out.println("maximum length : "+max);
+		System.out.println("final string : ");
+		for (int i = 0; i < a2.length; i++) {
+			System.out.print(a1[i]);
 		}
+		System.out.println();
+		System.out.print("final short string : ");
+		for (int i = 0; i < p; i++) {
+			System.out.print(a3[i]);
+			
+		}
+		
+		
+		
 	}
 }
+
